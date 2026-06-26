b2528e62068a9fe33d2f9db55f772acaf804ba2d
milaptop <usr235@milaptop235.local>
2026-06-26 10:42:23 -0500

    agregado supabase a requirements

--- user_repository.py
+++ user_repository.py
@@ -1,13 +1,19 @@
 # user_repository.py
+import os
+from supabase import create_client
 
 class UserRepository:
     def __init__(self):
-        # Simulación de una base de datos de usuarios (En producción, las contraseñas estarían hasheadas)
-        self._database = {
-            "admin": {"password": "12345", "name": "Admi"},
-            "user1": {"password": "securepass", "name": "Juan"}
-        }
+        url = os.environ.get("https://kdwdvyuwfrywslzzmrrc.supabase.co")
+        key = os.environ.get("sb_publishable__gt1TFFxjz2-FS8FJJZhcw_6J3dHgWU")
+        self.supabase = create_client(url, key)
 
     def get_user_by_username(self, username: str) -> dict | None:
-        """Busca un usuario en la 'base de datos'. Devuelve el usuario o None."""
-        return self._database.get(username, None)
+        response = self.supabase.table("users") \
+            .select("*") \
+            .eq("username", username) \
+            .execute()
+        
+        if response.data:
+            return response.data[0]
+        return None
