f2475395ab7aab3323a63326c28946bb8d318885
milaptop <usr235@milaptop235.local>
2026-06-26 10:23:51 -0500

    Update: print terminal en inicio de sesion exitoso

--- auth_service.py
+++ auth_service.py
@@ -6,18 +6,13 @@ class AuthService:
         self.user_repo = UserRepository()
 
     def login(self, username: str, password: str) -> dict | None:
-        """
-        Verifica las credenciales.
-        Devuelve los datos del usuario si son correctos, o None si falla.
-        """
-        # Validaciones de seguridad básicas antes de ir a la "BD"
         if not username or not password:
             return None
             
         user = self.user_repo.get_user_by_username(username)
         
         if user and user["password"] == password:
-            # Retornamos la información del usuario (menos el password por seguridad)
+            print(f"✅ Usuario '{username}' inició sesión correctamente")
             return {"username": username, "name": user["name"]}
             
         return None
